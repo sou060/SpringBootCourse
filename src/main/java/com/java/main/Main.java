adbb728fd3dd73fae788e0dee10ec41005bee4ee
Sourav Mondal <souravmondal060@gmail.com>
2025-04-08 08:45:00 +0530

    SpringCoreXMl Approach bean created and object passed throuh the property tag

--- src/main/java/com/java/main/Main.java
+++ src/main/java/com/java/main/Main.java
@@ -1,5 +1,7 @@
 package com.java.main;
 
+import com.java.service.ICourse;
+import com.java.service.Telusko;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,5 +10,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationconfig.xml");
+        Telusko telusko = (Telusko) context.getBean("telusko");
+        //telusko.setCourse((ICourse) context.getBean("springBootCourse")); In Xml the object is passing
+        if (telusko.isCourseAvailable()) {
+            System.out.println("Course is available");
+            telusko.getCourseName();
+            telusko.getDuration();
+            telusko.getPrice();
+            telusko.getRating();
+            telusko.getDescription();
+            telusko.getInstructorName();
+            telusko.getCourseContent();
+            telusko.getCourseLevel();
+            telusko.getCourseLanguage();
+            telusko.getCourseType();
+        } else {
+            System.out.println("No course available");
         }
     }
+}
--- src/main/java/com/java/service/Telusko.java
+++ src/main/java/com/java/service/Telusko.java
@@ -5,6 +5,83 @@ public class Telusko {
         System.out.println("Telusko Course");
 
     }
+  private ICourse iCourse;
+public void setCourse(ICourse iCourse)
+{
+    this.iCourse = iCourse;
+}
+public boolean isCourseAvailable() {
+    return iCourse != null;
 
+}
+public void getCourseName() {
+    if (iCourse != null) {
+        iCourse.getCourseName();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getDuration() {
+    if (iCourse != null) {
+        iCourse.getDuration();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getPrice() {
+    if (iCourse != null) {
+        iCourse.getPrice();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getRating() {
+    if (iCourse != null) {
+        iCourse.getRating();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getDescription() {
+    if (iCourse != null) {
+        iCourse.getDescription();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getInstructorName() {
+    if (iCourse != null) {
+        iCourse.getInstructorName();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getCourseContent() {
+    if (iCourse != null) {
+        iCourse.getCourseContent();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getCourseLevel() {
+    if (iCourse != null) {
+        iCourse.getCourseLevel();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getCourseLanguage() {
+    if (iCourse != null) {
+        iCourse.getCourseLanguage();
+    } else {
+        System.out.println("No course available");
+    }
+}
+public void getCourseType() {
+    if (iCourse != null) {
+        iCourse.getCourseType();
+    } else {
+        System.out.println("No course available");
+    }}
 
 }
